5c99d974314eea86863e7032cb0a8d81de6e818d
Dan Shechter <dans@houmus.org>
2019-09-26 07:32:06 +0300

    Solutions

--- exercises/Packing/Ex04/Decimalize.cs
+++ exercises/Packing/Ex04/Decimalize.cs
@@ -22,7 +22,76 @@ namespace Packing.Ex04
 
         static unsafe (int exp, int[] ints) DecimalizeVectorizedInPlace(float *dataPtr, int length)
         {
-            throw new NotImplementedException();
+            bool IsRoundedProperly(float* pf, int length, float epsilon)
+            {
+                var vectorizedLen = length / 8;
+                var end = pf + length;
+
+                var epsPlusVec = Vector256.Create(epsilon);
+                var epsMinusVec = Vector256.Create(-epsilon);
+                for (var i = 0; i < vectorizedLen; i++, pf += 8)
+                {
+                    var vec = Avx.LoadVector256(pf);
+                    var roundedVec = Avx.RoundToNearestInteger(vec);
+                    var diffVec = Avx.Subtract(vec, roundedVec);
+                    var c1 = Avx.Compare(diffVec, epsPlusVec, FloatComparisonMode.OrderedGreaterThanOrEqualNonSignaling);
+                    var c2 = Avx.Compare(diffVec, epsMinusVec, FloatComparisonMode.OrderedLessThanOrEqualNonSignaling);
+                    var res = Avx.Or(c1, c2);
+                    var resScalar = Avx.MoveMask(res);
+                    if (resScalar > 0)
+                        return false;
+                }
+                
+                for (; pf < end; pf++)
+                    if (Abs((Round(*pf)) - *pf) > epsilon)
+                        return false;
+
+                return true;
+            }
+
+            void MultiplyBy(float* pf, int length, float mul)
+            {
+                var vectorizedLen = length / 8;
+                var end = pf + length;
+
+                var mulVec = Vector256.Create(mul);
+                for (var i = 0; i < vectorizedLen; i++, pf += 8)
+                {
+                    var vec = Avx.LoadVector256(pf);
+                    vec = Avx.Multiply(vec, mulVec);
+                    Avx.Store(pf, vec);
+                }
+
+                for (; pf < end; pf++)
+                    *pf *= mul;
+            }
+
+            var exp = 0;
+            while (!IsRoundedProperly(dataPtr, length, EPSILON))
+            {
+                MultiplyBy(dataPtr, length, 10);
+                exp++;
+            }
+
+            var vectorizedLen = length / 8;
+            var result = new int[length];
+            var pf = dataPtr;
+            var end = pf + length;
+            fixed (int* pResult = &result[0])
+            {
+                var pi = pResult;
+                for (var i = 0; i < vectorizedLen; i++, pf += 8, pi += 8) {
+                    var vec = Avx.LoadVector256(pf);
+                    var roundedVec = Avx.RoundToNearestInteger(vec);
+                    var intVec = Avx.ConvertToVector256Int32(roundedVec);
+                    Avx.Store(pi, intVec);
+                }
+                
+                while (pf < end)
+                    *pi++ = (int) Round(*pf++);
+            }
+
+            return (exp, result);
         }
 
         internal static (int exp, int[] ints) DecimalizeScalar(float[] dataPtr)
